6cd551a1c8a067afdaa987cf2002dc53f66fa805
HernanBergero <hernanbergero@meyerbergero.com.ar>
2023-08-08 17:13:24 -0300

    Cambio nombre
    Cambio a chau mundo

--- src/ejemplo345433/Ejemplo345433.java
+++ src/ejemplo345433/Ejemplo345433.java
@@ -14,7 +14,7 @@ public class Ejemplo345433 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo.!!!");
+        System.out.println("Chau mundo.!!!");
     }
     
 }
