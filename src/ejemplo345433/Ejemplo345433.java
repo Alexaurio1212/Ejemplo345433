4d29ed42d7161e16e11fe5c70fdf021c93e1cc77
soviedo1983 <141676216+soviedo1983@users.noreply.github.com>
2023-08-08 18:14:55 -0300

    Update Ejemplo345433.java

--- src/ejemplo345433/Ejemplo345433.java
+++ src/ejemplo345433/Ejemplo345433.java
@@ -1,18 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package ejemplo345433;
 
-/**
- *
- * @author usuario
- */
 public class Ejemplo345433 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Chau mundo.!!!");
     }
